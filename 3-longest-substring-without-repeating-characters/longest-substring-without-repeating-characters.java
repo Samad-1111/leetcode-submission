1024f1a67c3e1aaaa4f002345aff188f976fe711
Samad-1111 <kasim.kasim2826@gmail.com>
2026-08-22 22:53:03 +0530

    Time: 164 ms (5.01%) | Memory: 47.8 MB (31.66%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int count,res = 0;
+        for(int i =0;i<s.length();i++){
+            count = 0;
+            boolean[] arr = new boolean [256];
+
+            for(int j=i;j<s.length();j++){
+                if(arr[s.charAt(j)] == true){
+                    break;
+                }else{
+                    count++;
+                    arr[s.charAt(j)] = true;
+                }
+            }res = Math.max(res,count);
+        }return res;
+    }
+}
