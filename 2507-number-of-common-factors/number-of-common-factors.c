d9468ccd15155a9690b4992fe202340637e61b2b
Samad-1111 <kasim.kasim2826@gmail.com>
2026-09-09 22:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (3.55%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.c
@@ -0,0 +1,10 @@
+int commonFactors(int a, int b) {
+    int count=0;
+    int min;
+    if(a<b) min =a;
+    min=b;
+    
+    for(int i=1;i<=min;i++){
+        if(a%i ==0 && b%i ==0) count++;
+    }return count;
+}
